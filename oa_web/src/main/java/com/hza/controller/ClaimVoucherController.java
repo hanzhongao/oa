9563cd7e1adb1d9d63836dc8aa25de6f750ed084
hza <hanzhongao@outlook.com>
2019-11-08 10:53:06 +0800

    待处理报销单按钮右侧是否显示 new 改 bug

--- oa_web/src/main/java/com/hza/controller/ClaimVoucherController.java
+++ oa_web/src/main/java/com/hza/controller/ClaimVoucherController.java
@@ -24,54 +24,58 @@ import java.util.Map;
 public class ClaimVoucherController {
 
     @Autowired
-    private ClaimVoucherBiz claimVoucherBiz ;
+    private ClaimVoucherBiz claimVoucherBiz;
 
     /**
      * 添加报销单前准备
+     *
      * @param map
      * @return
      */
     @RequestMapping("/to_add")
-    public String toAdd(Map<String,Object> map) {
-        map.put("costTypes", Content.getCostTypes()) ;
-        map.put("info", new ClaimVoucherInfo()) ;
-        return "claim_voucher_add" ;
+    public String toAdd(Map<String, Object> map) {
+        map.put("costTypes", Content.getCostTypes());
+        map.put("info", new ClaimVoucherInfo());
+        return "claim_voucher_add";
     }
 
     /**
      * 添加报销单
+     *
      * @param session
      * @param info
      * @return
      */
     @RequestMapping("/add")
     public String add(HttpSession session, ClaimVoucherInfo info) {
-        Employee employee = (Employee) session.getAttribute("employee") ;
+        Employee employee = (Employee) session.getAttribute("employee");
 
         info.getClaimVoucher().setCreateSn(employee.getSn()); // 设置创建者编号
         info.getClaimVoucher().setNextDealSn(employee.getSn()); // 设置待处理者编号，下一个待处理者为创建者本人
 
         this.claimVoucherBiz.add(info.getClaimVoucher(), info.getItems()); // 保存报销单
 
-        return "redirect:deal" ;
+        return "redirect:deal";
     }
 
     /**
      * 查看个人报销单
+     *
      * @param session
      * @param map
      * @return
      */
     @RequestMapping("/self")
-    public String self(HttpSession session, Map<String,Object> map) {
-        Employee employee = (Employee) session.getAttribute("employee") ;
-        map.put("lists", this.claimVoucherBiz.getByCreater(employee.getSn()).get("list")) ;
+    public String self(HttpSession session, Map<String, Object> map) {
+        Employee employee = (Employee) session.getAttribute("employee");
+        map.put("lists", this.claimVoucherBiz.getByCreater(employee.getSn()).get("list"));
 //        map = this.claimVoucherBiz.getByCreater(employee.getSn());
-        return "claim_voucher_self" ;
+        return "claim_voucher_self";
     }
 
     /**
      * 报销单详细信息
+     *
      * @param id
      * @param map
      * @return
@@ -79,29 +83,31 @@ public class ClaimVoucherController {
     @RequestMapping("/detail")
     public String detail(Integer id, Map<String, Object> map) {
 
-        Map<String, Object> res = this.claimVoucherBiz.get(id) ;
+        Map<String, Object> res = this.claimVoucherBiz.get(id);
 
-        map.put("claimVoucher",res.get("claimVoucher")) ;
-        map.put("claimVoucherItems",res.get("claimVoucherItems")) ;
-        map.put("dealRecordes",res.get("dealRecordes")) ;
+        map.put("claimVoucher", res.get("claimVoucher"));
+        map.put("claimVoucherItems", res.get("claimVoucherItems"));
+        map.put("dealRecordes", res.get("dealRecordes"));
         // claimVoucher     -> 查询报销单信息
         // claimVoucherItems -> 查询报销单详情信息
         // dealRecordes       -> 查询处理记录
 
-        return "claim_voucher_detail" ;
+        return "claim_voucher_detail";
     }
 
     /**
      * 根据 待处理人 查询报销单
+     *
      * @param session
      * @return
      */
     @RequestMapping("/deal")
-    public String deal(HttpSession session, Map<String,Object> map) {
+    public String deal(HttpSession session, Map<String, Object> map) {
 
 
-        Employee employee = (Employee) session.getAttribute("employee") ;
+        Employee employee = (Employee) session.getAttribute("employee");
         List<ClaimVoucher> list = this.claimVoucherBiz.getByDealer(employee.getSn());
+        map.put("claimVouchers", list); // 查询所有当前登录用户的待处理报销单
 
         // 待处理报销单按钮右侧是否显示 new
         if (list.size() > 0) {
@@ -110,51 +116,50 @@ public class ClaimVoucherController {
             session.setAttribute("showNewPic", false);
         }
 
-        map.put("claimVouchers", list) ; // 查询所有当前登录用户的待处理报销单
 
-
-
-
-        return "claim_voucher_deal" ;
+        return "claim_voucher_deal";
     }
 
     /**
      * 提交报销单
+     *
      * @param id
      * @return
      */
     @RequestMapping("/submit")
     public String submit(Integer id) {
         this.claimVoucherBiz.submit(id);
-        return "redirect:deal" ;
+        return "redirect:deal";
     }
 
     /**
      * 跳转到审核页面
+     *
      * @param id
      * @param map
      * @return
      */
     @RequestMapping("/to_check")
     public String toCheck(Integer id, Map<String, Object> map) {
-        Map<String, Object> res = this.claimVoucherBiz.get(id) ;
+        Map<String, Object> res = this.claimVoucherBiz.get(id);
 
-        map.put("claimVoucher",res.get("claimVoucher")) ;
-        map.put("claimVoucherItems",res.get("claimVoucherItems")) ;
-        map.put("dealRecordes",res.get("dealRecordes")) ;
+        map.put("claimVoucher", res.get("claimVoucher"));
+        map.put("claimVoucherItems", res.get("claimVoucherItems"));
+        map.put("dealRecordes", res.get("dealRecordes"));
 
         DealRecord dealRecord = new DealRecord();
         dealRecord.setClaimVoucherId(id);
-        map.put("dealRecord", dealRecord) ;
+        map.put("dealRecord", dealRecord);
         // claimVoucher     -> 查询报销单信息
         // claimVoucherItems -> 查询报销单详情信息
         // dealRecordes       -> 查询处理记录
 
-        return "claim_voucher_check" ;
+        return "claim_voucher_check";
     }
 
     /**
      * 审核操作
+     *
      * @param dealRecord 处理记录
      * @return
      */
@@ -162,59 +167,61 @@ public class ClaimVoucherController {
     public String check(HttpSession session, DealRecord dealRecord) {
 
         // 获取当前登录用户
-        Employee employee = (Employee) session.getAttribute("employee") ;
+        Employee employee = (Employee) session.getAttribute("employee");
 
         // 设置处理人
         dealRecord.setDealSn(employee.getSn());
         dealRecord.setDealer(employee);
 
         // 调用审核业务
-        this.claimVoucherBiz.check(dealRecord) ;
+        this.claimVoucherBiz.check(dealRecord);
 
         // 返回待处理页面
-        return "redirect:deal" ;
+        return "redirect:deal";
     }
 
     @RequestMapping("/pay")
     public String pay(HttpSession session, Integer id) {
 
         // 获取当前登录用户
-        Employee employee = (Employee) session.getAttribute("employee") ;
+        Employee employee = (Employee) session.getAttribute("employee");
 
 
-        DealRecord dealRecord = new DealRecord() ;
+        DealRecord dealRecord = new DealRecord();
         // 设置处理人
         dealRecord.setDealSn(employee.getSn());
         dealRecord.setClaimVoucherId(id);
 
         this.claimVoucherBiz.pay(dealRecord);
-        return "redirect:deal" ;
+        return "redirect:deal";
 
     }
 
     /**
      * 修改报销单前准备
+     *
      * @param map
      * @return
      */
     @RequestMapping("/to_update")
-    public String toUpdate(Integer id , Map<String,Object> map) {
+    public String toUpdate(Integer id, Map<String, Object> map) {
 
-        Map<String, Object> res = this.claimVoucherBiz.get(id) ;
+        Map<String, Object> res = this.claimVoucherBiz.get(id);
 
-        ClaimVoucher claimVoucher = (ClaimVoucher) res.get("claimVoucher") ;
-        List<ClaimVoucherItem> items = (List<ClaimVoucherItem>) res.get("claimVoucherItems") ;
+        ClaimVoucher claimVoucher = (ClaimVoucher) res.get("claimVoucher");
+        List<ClaimVoucherItem> items = (List<ClaimVoucherItem>) res.get("claimVoucherItems");
 
-        map.put("costTypes", Content.getCostTypes()) ;
+        map.put("costTypes", Content.getCostTypes());
         ClaimVoucherInfo info = new ClaimVoucherInfo();
         info.setClaimVoucher(claimVoucher);
         info.setItems(items);
-        map.put("info", info) ;
-        return "claim_voucher_update" ;
+        map.put("info", info);
+        return "claim_voucher_update";
     }
 
     /**
      * 添加报销单
+     *
      * @param session
      * @param info
      * @return
@@ -222,14 +229,14 @@ public class ClaimVoucherController {
     @RequestMapping("/update")
     public String update(HttpSession session, ClaimVoucherInfo info) {
 
-        Employee employee = (Employee) session.getAttribute("employee") ;
+        Employee employee = (Employee) session.getAttribute("employee");
 
         info.getClaimVoucher().setCreateSn(employee.getSn()); // 设置创建者编号
         info.getClaimVoucher().setNextDealSn(employee.getSn()); // 设置待处理者编号，下一个待处理者为创建者本人
 
         this.claimVoucherBiz.update(info.getClaimVoucher(), info.getItems()); // 修改报销单
 
-        return "redirect:deal" ;
+        return "redirect:deal";
     }
 
 }
--- oa_web/src/main/java/com/hza/controller/SelfController.java
+++ oa_web/src/main/java/com/hza/controller/SelfController.java
@@ -1,12 +1,15 @@
 package com.hza.controller;
 
+import com.hza.biz.ClaimVoucherBiz;
 import com.hza.biz.SelfBiz;
+import com.hza.entity.ClaimVoucher;
 import com.hza.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 /**
  * Create by hza
@@ -16,19 +19,23 @@ import javax.servlet.http.HttpSession;
 public class SelfController {
 
     @Autowired
-    private SelfBiz selfBiz ;
+    private SelfBiz selfBiz;
+    @Autowired
+    private ClaimVoucherBiz claimVoucherBiz ;
 
     /**
      * 跳转到登录页面
      * @return
      */
     @RequestMapping("/to_login")
-    public String toLogin(){
-        return "login" ;
+
+    public String toLogin() {
+        return "login";
     }
 
     /**
      * 登录
+     *
      * @param session
      * @param sn
      * @param password
@@ -38,57 +45,71 @@ public class SelfController {
     public String login(HttpSession session, String sn, String password) {
 
         // 调用业务层登录方法
-        Employee employee = this.selfBiz.login(sn, password) ;
+        Employee employee = this.selfBiz.login(sn, password);
+
 
         if (employee != null) {                            // 登录成功
             session.setAttribute("employee", employee); // 将用户信息写入session
-            return "redirect:self" ;                       // 重定向到个人信息页面
+            List<ClaimVoucher> list = this.claimVoucherBiz.getByDealer(employee.getSn());
+
+            // 待处理报销单按钮右侧是否显示 new
+            if (list.size() > 0) {
+                session.setAttribute("showNewPic", true);
+            } else {
+                session.setAttribute("showNewPic", false);
+            }
+
+            return "redirect:self";                       // 重定向到个人信息页面
         } else {                                           // 登录失败
-            return "redirect:to_login" ;                   // 跳转到登录页面
+            return "redirect:to_login";                   // 跳转到登录页面
         }
     }
 
     /**
      * 用户登出
+     *
      * @return
      */
     @RequestMapping("/quit")
     public String quit(HttpSession session) {
         session.removeAttribute("employee"); // 清空 session
-        return "redirect:to_login" ; // 跳转到登录页面
+        return "redirect:to_login"; // 跳转到登录页面
     }
 
     /**
      * 跳转到个人信息
+     *
      * @return
      */
     @RequestMapping("/self")
     public String toSelf() {
-        return "self" ;
+        return "self";
     }
 
     /**
      * 跳转到修改密码页面
+     *
      * @return
      */
     @RequestMapping("/to_change_password")
-    public String toChangePassword(){
-        return "change_password" ;
+    public String toChangePassword() {
+        return "change_password";
     }
 
     /**
      * 修改密码
-     * @param session  获取session
-     * @param old  旧密码
-     * @param new1 新密码
-     * @param new2 重复对照
+     *
+     * @param session 获取session
+     * @param old     旧密码
+     * @param new1    新密码
+     * @param new2    重复对照
      * @return
      */
     @RequestMapping("change_password")
     public String changePassword(HttpSession session, String old, String new1, String new2) {
 
         // 从 session 中获取用户实例
-        Employee employee = (Employee) session.getAttribute("employee") ;
+        Employee employee = (Employee) session.getAttribute("employee");
 
         if (employee.getPassword().equals(old)) {  // 密码输入正确
             if (new1.equals(new2)) { // 重复输入新密码正确
@@ -97,11 +118,11 @@ public class SelfController {
                 employee.setPassword(new1);
                 this.selfBiz.changePassword(employee);
 
-                return "redirect:quit" ; // 修改完毕登出
+                return "redirect:quit"; // 修改完毕登出
             }
         }
 
         // 修改失败，跳转到 修改密码页面
-        return "redirect:to_change_password" ;
+        return "redirect:to_change_password";
     }
 }
